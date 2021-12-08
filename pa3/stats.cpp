3f4f52ee570e9edd29b4387d7b047ba8910eea88
John Matson <john@matson.ai>
2021-12-07 20:57:38 -0800

    stats contructor, getAvg implemented

--- pa3/stats.cpp
+++ pa3/stats.cpp
@@ -1,22 +1,53 @@
 
 #include "stats.h"
+// #include <math.h>
 
 stats::stats(PNG & im){
 
-/* your code here! */
+    for(int x = 0; x < im.width(); x++) {
+        for(int y = 0; y < im.height(); y++) {
+
+            sumHueX[x][y] = 0;
+            sumHueY[x][y] = 0;
+            sumSat[x][y] = 0;
+            sumLum[x][y] = 0;
+
+            for(int xSum = 0; xSum < x; xSum++) {
+                for(int ySum = 0; ySum < y; ySum++) {
+
+                    HSLAPixel *pixel = im.getPixel(xSum, ySum);
+
+                    double hRad = pixel->h * PI / 180;
+                    sumHueX[x][y] += cos(hRad) * 180 / PI;
+                    sumHueY[x][y] += sin(hRad) * 180 / PI;
+                    sumSat[x][y] += pixel->s;
+                    sumLum[x][y] += pixel->l;
+                }
+            }
+        }
+    }
 
 }
 
 long stats::rectArea(pair<int,int> ul, pair<int,int> lr){
 
-return ((lr.first-ul.first)+1)*((lr.second-ul.second)+1);
+    return ((lr.first-ul.first)+1)*((lr.second-ul.second)+1);
 
 }
 
 HSLAPixel stats::getAvg(pair<int,int> ul, pair<int,int> lr){
 
-/* your code here */
+    HSLAPixel avg = HSLAPixel();
 
+    double xHue, yHue;
+    
+    xHue = sumHueX[ul.first-1][ul.second-1] - sumHueX[lr.first][lr.second] / rectArea(ul,lr);
+    yHue = sumHueY[ul.first-1][ul.second-1] - sumHueY[lr.first][lr.second] / rectArea(ul,lr);
+    
+    avg.h = atan2(yHue, xHue);
+    avg.s = sumSat[ul.first-1][ul.second-1] - sumSat[lr.first][lr.second] / rectArea(ul,lr);
+    avg.l = sumLum[ul.first-1][ul.second-1] - sumLum[lr.first][lr.second] / rectArea(ul,lr);
+    
 }
 
 double stats::entropy(pair<int,int> ul, pair<int,int> lr){
